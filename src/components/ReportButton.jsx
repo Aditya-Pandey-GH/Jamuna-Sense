e9d5a2e50be2a9d69ce573a65141cafb5d37099d
Tushar Kumar <tusharrr0302@Tushars-MacBook-Air.local>
2026-02-08 09:14:47 +0530

    init

--- src/components/ReportButton.jsx
+++
@@ -1,9 +0,0 @@
-import "./ReportButton.css";
-
-export default function ReportButton({ onClick, children = "Report Issue" }) {
-	return (
-		<button className="report-button" onClick={onClick}>
-			{children}
-		</button>
-	);
-}
